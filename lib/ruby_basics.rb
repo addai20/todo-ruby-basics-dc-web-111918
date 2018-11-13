b33627cd32897406f4c62aeb4beaf2d6ac65d608
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-13 16:44:25 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
+  variable = value
+end
+
+def argue(argmuent)
+  argmuent
+end
+
+def greeting (name,like)
+  puts "Hello! My name is #{name} and I like #{like}!"
   
 end
 
-def argue
-  
+def return_a_value(phrase="Nice")
+  phrase
 end
 
-def greeting
-
+def last_evaluated_value(phrase="expert")
+  phrase
 end
 
-def return_a_value
-
-end
-
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party(topping="cheese")
+  topping
 end
